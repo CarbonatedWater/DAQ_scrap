6c48e1b627138dbbda922a2f46c9115875c58e4c
CarbonatedWater <freelunacy@gmail.com>
2020-04-04 11:09:37 +0900

    네이버 이미지 링크 오류 수정

--- scrap/sbs.py
+++ scrap/sbs.py
@@ -66,7 +66,7 @@ def scrap(prog_name, url, original_air_date, week):
             air_date = str(parse(air_date_check).date())
             try:
                 preview_img = content_info.select_one('div.thumb > img')['src'].replace('quality=90', 'quality=100')\
-                    .replace('265x150', '530x300').replace(';', '')
+                    .replace('265x150', '530x300').replace(';=', '=')
                 preview_img = unquote(preview_img)
             except:
                 print('===== naver img None')
