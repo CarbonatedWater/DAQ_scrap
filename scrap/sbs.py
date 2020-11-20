65cf3d4af7577374300b0eca471960af3d35084a
CarbonatedWater <freelunacy@gmail.com>
2020-11-21 00:28:09 +0900

    SBS 꼬꼬무 추가

--- scrap/sbs.py
+++ scrap/sbs.py
@@ -17,7 +17,8 @@ API_URL = {
     'SBS 스페셜': 'sbsspecial', 
     '그것이 알고싶다': 'unansweredquestions', 
     '궁금한 이야기 Y': 'cube', 
-    '순간포착 세상에 이런일이': 'whatonearth'
+    '순간포착 세상에 이런일이': 'whatonearth', 
+    '꼬리에 꼬리를 무는 그날 이야기': '2020tail'
 }
 
 BTV_CON_ID = {
@@ -32,7 +33,8 @@ def scrap(prog_name, url, original_air_date, week):
     resp = s.get("https://static.apis.sbs.co.kr/program-api/2.0/main/" + API_URL[prog_name])
     #soup = BeautifulSoup(resp.text, 'lxml')
     content = json.loads(resp.text)
-    if prog_name in ["궁금한 이야기 Y", "그것이 알고싶다", '순간포착 세상에 이런일이']:
+    if prog_name in ["궁금한 이야기 Y", "그것이 알고싶다", '순간포착 세상에 이런일이', \
+        '꼬리에 꼬리를 무는 그날 이야기']:
         show_advance = content['layers'][3]['items'][0]['medias'] # 미리보기 리스트
     elif prog_name == "SBS 스페셜":
         show_advance = content['layers'][3]['items'][2]['medias']
@@ -44,16 +46,18 @@ def scrap(prog_name, url, original_air_date, week):
     except:
         air_date = str(parse(show_advance[0]['broaddate']).date())
 
-    if prog_name == '순간포착 세상에 이런일이':
-        title = show_advance[0]['title']
+    if prog_name in ['순간포착 세상에 이런일이', '꼬리에 꼬리를 무는 그날 이야기']:
+        if prog_name == '꼬리에 꼬리를 무는 그날 이야기':
+            title = show_advance[0]['contenttitle']
+        else:
+            title = show_advance[0]['title']
         preview_img = show_advance[0]['thumb']['large']
         preview_mov = None
         description = show_advance[0]['description']
-        # 네이버 검색 노출 정보 반영(회차)
+        # 다음 TV 정보 반영(회차)
         result_daum = utils.get_daum_info(prog_name)
         air_num = result_daum['air_num']
         air_date = result_daum['air_date']
-        preview_mov = ''
     else:
         air_num_tmp = re.search(r"([0-9]+)회", show_advance[0]['title'])
         if not(air_num_tmp):
