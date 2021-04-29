7086f7ba18aa610dec739bc1a0c127f6b88b73e1
CarbonatedWater <freelunacy@gmail.com>
2021-04-29 21:59:17 +0900

    jtbc 홈페이지 스포트라이트 버그 우회

--- scrap/jtbc.py
+++ scrap/jtbc.py
@@ -19,45 +19,57 @@ def scrap(prog_name, url, original_air_date, week):
     s = utils.sess(REFER)
     resp = s.get(url)
     soup = BeautifulSoup(resp.text, 'lxml')
-    if prog_name == '이규연의 스포트라이트':
-        content = soup.select_one('div.bx_preview')
-        tmp_date = content.select_one('span.date').text
-        air_date = re.match(r"^\d{4}(\.\d{2}){2}", tmp_date).group().replace(".", "-")
-        air_num = re.search(r"([0-9]+)회", content.select_one('span.tit').text).group(1)
-        title = [x for x in content.select_one('span.info span.txt').children][0].replace("부제 : ", "").replace("'", "")
-        preview_img = content.select_one('span.img > img')['src']
-        preview_mov = None
-        description = content.select_one('span.txt').text
-    elif prog_name == '차이나는 클라스':
-        #content = soup.select_one('[data-host]')
-        tmp_date = soup.select_one('div.play_info em').text
-        air_date = re.match(r"^\d{4}(\.\d{2}){2}", tmp_date).group().replace(".", "-")
-        title = soup.find("meta", property="og:title")['content']
-        try:
-            air_num = re.search(r"([0-9]+)회", title).group(1)
-        except:
-            return None
-        preview_img = soup.find("meta", property="og:image")['content']
-        preview_mov = None
-        description = soup.find("meta", {'name': "og:description"})['content']
-    elif prog_name in ['다큐 플러스', 'JTBC FACTUAL']:
-        content = soup.select_one('div.bx_preview')
-        tmp_date = content.select_one('span.date').text
-        air_date = re.match(r"^\d{4}(\.\d{2}){2}", tmp_date).group().replace(".", "-")
-        if prog_name == '다큐 플러스':
-            title = content.select_one('span.info span.txt').text
-        else:
-            title = content.select_one('span.info span.tit').text
-        preview_img = content.select_one('span.img > img')['src']
-        # 회차가 없음
-        air_num = re.search(r"img\/([0-9]{8})", preview_img).group(1)
-        preview_mov = None
-        description = content.select_one('span.txt').text
-    
+    try:
+        if prog_name == '이규연의 스포트라이트':
+            content = soup.select_one('div.bx_preview')
+            tmp_date = content.select_one('span.date').text
+            air_date = re.match(r"^\d{4}(\.\d{2}){2}", tmp_date).group().replace(".", "-")
+            air_num = re.search(r"([0-9]+)회", content.select_one('span.tit').text).group(1)
+            title = [x for x in content.select_one('span.info span.txt').children][0].replace("부제 : ", "").replace("'", "")
+            preview_img = content.select_one('span.img > img')['src']
+            preview_mov = None
+            description = content.select_one('span.txt').text
+        elif prog_name == '차이나는 클라스':
+            #content = soup.select_one('[data-host]')
+            tmp_date = soup.select_one('div.play_info em').text
+            air_date = re.match(r"^\d{4}(\.\d{2}){2}", tmp_date).group().replace(".", "-")
+            title = soup.find("meta", property="og:title")['content']
+            try:
+                air_num = re.search(r"([0-9]+)회", title).group(1)
+            except:
+                return None
+            preview_img = soup.find("meta", property="og:image")['content']
+            preview_mov = None
+            description = soup.find("meta", {'name': "og:description"})['content']
+        elif prog_name in ['다큐 플러스', 'JTBC FACTUAL']:
+            content = soup.select_one('div.bx_preview')
+            tmp_date = content.select_one('span.date').text
+            air_date = re.match(r"^\d{4}(\.\d{2}){2}", tmp_date).group().replace(".", "-")
+            if prog_name == '다큐 플러스':
+                title = content.select_one('span.info span.txt').text
+            else:
+                title = content.select_one('span.info span.tit').text
+            preview_img = content.select_one('span.img > img')['src']
+            # 회차가 없음
+            air_num = re.search(r"img\/([0-9]{8})", preview_img).group(1)
+            preview_mov = None
+            description = content.select_one('span.txt').text
+    except Exception as e:
+        print(f"===== error: {e}")
+        pass
     # DAUM 정보 보완
     result_daum = utils.get_daum_info(prog_name)
-    if result_daum and result_daum['air_date'] == air_date:
+    try:
+        if result_daum and result_daum['air_date'] == air_date:
+            description = result_daum['desc']
+    except:
+        air_date = result_daum['air_date']
+        air_num = result_daum['air_num']
+        title = result_daum['sub_title']
+        preview_img = result_daum['preview_img']
+        preview_mov = ''
         description = result_daum['desc']
+
 
     result = {
         'air_date': air_date, 
